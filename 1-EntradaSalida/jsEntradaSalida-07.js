8105d1f21f4a12917a409b617683ed0211bacb92
Camila Icardi <118855224+camilaicardi@users.noreply.github.com>
2023-02-03 15:59:00 -0300

    Add files via upload
    ejerc del 1 al 6

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,12 +5,42 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	var num1;
+	var num2;
+	var resultado;
+
+	num1 = document.getElementById("txtIdNumeroUno").value;
+	
+	num1 = parseInt(num1); 
+
+
+	num2 = document.getElementById("txtIdNumeroDos").value;
+	
+	num2 = parseInt(num2);
+	
+	
+	resultado = num1 + num2;
+	
+	alert("la suma es " + resultado);
 }
 
 function restar()
 {
 	
+
+	num1 = document.getElementById("txtIdNumeroUno").value;
+	
+	num1 = parseInt(num1); 
+
+
+	num2 = document.getElementById("txtIdNumeroDos").value;
+	
+	num2 = parseInt(num2);
+	
+	
+	resultado = num1 - num2;
+
+	alert("la suma es " + resultado);
 }
 
 function multiplicar()
