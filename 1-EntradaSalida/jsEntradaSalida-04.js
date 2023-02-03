557f48016691c3c7723726cf7b1371b47fa6661c
Camila Icardi <118855224+camilaicardi@users.noreply.github.com>
2023-02-03 15:22:55 -0300

    Add files via upload
    ejerci resueltos 1 al 5

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -3,7 +3,19 @@ Debemos lograr tomar un dato por 'PROMPT'
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
-	alert("ok");
+{ 
+	// primer paso:
+	var nombre;
+
+// seg paso: al nombre le doy un significado
+ nombre = prompt("ingrese su nombre");
+
+// copio el nombre de la variable nombre dentro de la caja de texto html. con la fun documt y el id 
+ document.getElementById("txtIdNombre").value = nombre;
+	
+  
+
+ 
+
 }
 
